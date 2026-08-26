03f505da09ead9d906d4a7755350a14219aee35e
KamlendraSingh <kamlendrasingh910@gmail.com>
2026-08-26 23:51:46 +0530

    Time: 2 ms (75.88%), Space: 42.2 MB (76.06%) - LeetHub

---
+++ 2231-largest-number-after-digit-swaps-by-parity/2231-largest-number-after-digit-swaps-by-parity.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int largestInteger(int num) {
+        final String s = String.valueOf(num);
+
+        int ans = 0;
+
+        Queue<Integer>[] maxHeap = new Queue[2];
+
+        for(int i = 0; i < 2; ++i){
+            maxHeap[i] = new PriorityQueue<>(Comparator.reverseOrder());
+        }
+        for(final char c : s.toCharArray()){
+            final int digit = c - '0';
+            maxHeap[digit % 2].offer(digit);
+        }
+        for(final char c : s.toCharArray()){
+            final int i = c - '0' & 1;
+            ans = (ans * 10 + maxHeap[i].poll());
+        }
+        return ans;
+    }
+}
