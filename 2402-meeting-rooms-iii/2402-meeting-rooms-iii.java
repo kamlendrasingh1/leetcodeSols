6509f8695661155de9193d6d2e2c46667a52657a
KamlendraSingh <kamlendrasingh910@gmail.com>
2026-08-31 00:05:33 +0530

    Time: 73 ms (79%), Space: 146.1 MB (93.4%) - LeetHub

--- 2402-meeting-rooms-iii/2402-meeting-rooms-iii.java
+++ 2402-meeting-rooms-iii/2402-meeting-rooms-iii.java
@@ -1,7 +1,28 @@
+class RoomEndTimeDetails{
+    long endTime;
+    int idx;
+
+    public RoomEndTimeDetails(long _endTime, int _idx){
+        this.endTime = _endTime;
+        this.idx = _idx;
+    }
+}
 class Solution {
     public int mostBooked(int n, int[][] meetings) {
         Arrays.sort(meetings, (a, b) -> a[0] - b[0]);
-        long[] endTimesOfRooms = new long[n];
+        PriorityQueue<Integer> availableRooms = new PriorityQueue<>();
+        PriorityQueue<RoomEndTimeDetails> occupiedRooms = new PriorityQueue<>(
+            (a, b) -> {
+                if(a.endTime == b.endTime){
+                    return a.idx - b.idx;
+                }
+                return Long.compare(a.endTime, b.endTime);
+            }
+        );
+
+        for(int i = 0; i < n; i++){
+            availableRooms.add(i);
+        }
         int[] counts = new int[n];
 
         for(int i = 0; i < meetings.length; i++){
@@ -11,20 +32,24 @@ class Solution {
             int minEndTimeIdx = 0;
             Boolean isRoomAllocated = false;
 
-            for(int room = 0; room < n; room++){
-                if(startTime >= endTimesOfRooms[room]){
-                    endTimesOfRooms[room] = endTime;
-                    counts[room] = counts[room] + 1;
-                    isRoomAllocated = true;
+            while(!occupiedRooms.isEmpty()){
+                if(startTime >= occupiedRooms.peek().endTime){
+                    RoomEndTimeDetails rETD = occupiedRooms.poll();
+                    availableRooms.add(rETD.idx);
+                }else{
                     break;
                 }
-                if(endTimesOfRooms[room] < endTimesOfRooms[minEndTimeIdx]){
-                    minEndTimeIdx = room;
-                }
             }
-            if(!isRoomAllocated){
-                endTimesOfRooms[minEndTimeIdx] = endTimesOfRooms[minEndTimeIdx] + endTime - startTime;
-                counts[minEndTimeIdx] = counts[minEndTimeIdx] + 1;
+
+            if(availableRooms.isEmpty()){
+                RoomEndTimeDetails minEndTimeOccupiedDetails = occupiedRooms.poll();
+                minEndTimeOccupiedDetails.endTime = minEndTimeOccupiedDetails.endTime + (endTime - startTime);
+                occupiedRooms.add(new RoomEndTimeDetails(minEndTimeOccupiedDetails.endTime, minEndTimeOccupiedDetails.idx));
+                counts[minEndTimeOccupiedDetails.idx] = counts[minEndTimeOccupiedDetails.idx] + 1;
+                }else{
+                int room = availableRooms.poll();
+                counts[room] = counts[room] + 1;
+                occupiedRooms.add(new RoomEndTimeDetails(endTime, room));
             }
         }
         int maxRoomsCountsIdx = 0;
