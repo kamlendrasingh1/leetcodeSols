1f8630433c1c92939c34765eb976adcb5b95380a
KamlendraSingh <kamlendrasingh910@gmail.com>
2026-09-25 23:46:51 +0530

    Time: 19 ms (33.61%), Space: 50.6 MB (85.73%) - LeetHub

--- 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.java
+++ 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.java
@@ -1,35 +1,40 @@
 class Solution {
     public int kthSmallest(int[][] matrix, int k) {
         int n = matrix.length;
-        int left = matrix[0][0];
-        int right = matrix[n - 1][n - 1];
-        int firstTrueIndex = -1;
+        PriorityQueue<int[]> minHeap = new PriorityQueue<>((a, b) -> a[0] - b[0]);
 
-        while(left <= right){
-            int mid = left + (right - left) / 2;
-            if(countLessEqual(matrix, mid, n) >= k){
-                firstTrueIndex = mid;
-                right = mid - 1;
-            } else{
-                left = mid + 1;
+        for(int i = 0; i < matrix.length; i = i + 1){
+            minHeap.add(new int[] {
+                matrix[i][0],
+                i,
+                0
+            });
+        }
+        int counter = 1;
+
+        while(!minHeap.isEmpty()){
+            int[] ele = minHeap.poll();
+
+            int val = ele[0];
+            int listId = ele[1];
+            int eleId = ele[2];
+
+            if(counter == k){
+                return val;
+            }
+
+            counter = counter + 1;
+
+            int nextEleId = eleId + 1;
+
+            if(nextEleId < n){
+                minHeap.add(new int[] {
+                    matrix[listId][nextEleId],
+                    listId,
+                    nextEleId
+                });
             }
         }
-        return firstTrueIndex;
-    }
-
-    private int countLessEqual(int[][] matrix, int target, int n){
-        int count = 0;
-        int row = n - 1;
-        int col = 0;
-
-        while(row >= 0 && col < n){
-            if(matrix[row][col] <= target){
-                count += row + 1;
-                col++;
-            } else{
-                row--;
-            }
-        }
-        return count;
+        return -1;
     }
 }
